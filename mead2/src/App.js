09b2bdcea2c1557e425189891413657cb65fc254
jbphinney <jbphinney@gmail.com>
2024-12-20 08:18:55 -0600

    Update App.js

--- mead2/src/App.js
+++ mead2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React change :D
+          React change :D another change
         </p>
         <a
           className="App-link"

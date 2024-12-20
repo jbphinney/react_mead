343e99796e9116c3a5481af3d471cea8ea31d9b1
jbphinney <jbphinney@gmail.com>
2024-12-20 08:21:56 -0600

    Update App.js

--- mead2/src/App.js
+++ mead2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React change :D another change
+          React change :D and now to test clean rebuild
         </p>
         <a
           className="App-link"
